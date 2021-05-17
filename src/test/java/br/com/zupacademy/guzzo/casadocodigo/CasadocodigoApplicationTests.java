4dd140bc2c5fa6f762be24b3119d6085471804b8
Rafael Guzzo <rafael.guzzo@zup.com.br>
2021-05-17 16:00:58 -0300

    setup inicial do projeto

---
+++ src/test/java/br/com/zupacademy/guzzo/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.guzzo.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
